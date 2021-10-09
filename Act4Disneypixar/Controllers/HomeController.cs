93baaf742b815eeeab99ef0b78d0d2de8aec0750
Ulises Tadeo Perez Garcia <ulisessykes8@gmail.com>
2021-10-08 21:26:38 -0500

    Controlador y vista Peliculas

---
+++ Act4Disneypixar/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Act4Disneypixar.Models;
+
+namespace Act4Disneypixar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Peliculas()
+        {
+            pixarContext context = new pixarContext();
+            var peliculas = context.Peliculas.OrderBy(x => x.Nombre);
+            return View(peliculas);
+        }
+    }
+}
