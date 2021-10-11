ca15e3ae59e7cb35607c5153abcd8061f60dc276
Ulises Tadeo Perez Garcia <ulisessykes8@gmail.com>
2021-10-11 16:59:14 -0500

    Cambios actuales

--- Act4Disneypixar/Controllers/HomeController.cs
+++ Act4Disneypixar/Controllers/HomeController.cs
@@ -15,11 +15,26 @@ namespace Act4Disneypixar.Controllers
             return View();
         }
 
+        [Route("peliculas")]
         public IActionResult Peliculas()
         {
             pixarContext context = new pixarContext();
             var peliculas = context.Peliculas.OrderBy(x => x.Nombre);
             return View(peliculas);
+        }
+
+        [Route("pelicula/{nombre}")]
+        public IActionResult InformacionPelicual(string nombre)
+        {
+            string nombre2 = nombre == null ? "" : nombre.Replace("-"," ");
+            pixarContext context = new pixarContext();
+            var pelicula = context.Peliculas.Include(x => x.Apariciones).ThenInclude(x => x.IdPersonajeNavigation)
+                .FirstOrDefault(x => x.Nombre == nombre2 || x.Nombre == nombre);
+            if (pelicula == null)
+            {
+                return RedirectToAction("peliculas");
+            }
+            return View(pelicula);
         }
         
        
