6c62912ddb179af49b0ed035b6fdf4b66dcdfea4
Ulises Tadeo Perez Garcia <ulisessykes8@gmail.com>
2021-10-22 20:23:02 -0500

    Ejemplos completos

--- Act5SweetBakery/Controllers/HomeController.cs
+++ Act5SweetBakery/Controllers/HomeController.cs
@@ -3,15 +3,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Act5SweetBakery.Models;
+using Act5SweetBakery.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Act5SweetBakery.Controllers
 {
     public class HomeController : Controller
     {
-        
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Route("productos")]
+        public IActionResult Productos()
+        {
+            sweetbakeryContext context = new sweetbakeryContext();
+            var productos = context.Categorias.Include(x => x.Productos).OrderBy(x => x.Nombre);
+            return View(productos);
+        }
+
+        [Route("recetas")]
+        public IActionResult Recetas()
+        {
+            sweetbakeryContext context = new sweetbakeryContext();
+            var recetas = context.Categorias.Include(x => x.Receta).Where(x => x.Receta.Any()).OrderBy(c => c.Nombre);
+            return View(recetas);
+        }
+
+        [Route("Receta/{nombre}")]
+        public IActionResult Receta(string nombre)
+        {
+            nombre = nombre.Replace("-", " ");
+            sweetbakeryContext context = new sweetbakeryContext();
+            var r = context.Recetas.FirstOrDefault(x => x.Nombre == nombre);
+            if (r == null)
+            {
+                return RedirectToAction("Recetas");
+            }
+            else
+            {
+                RecetaViewModel vm = new RecetaViewModel();
+                vm.Receta = r;
+
+                Random rnd = new Random();
+
+                vm.TresRecetas = context.Recetas
+                    .Where(x => x.Id != r.Id).ToList()
+                    .OrderBy(x => rnd.Next())
+                    .Take(3).Select(x => new Receta { Id = x.Id, Nombre = x.Nombre });
+                return View(vm);
+            }
+
         }
     }
 }
---
+++ Act5SweetBakery/Models/ViewModels/RecetaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Act5SweetBakery.Models.ViewModels
+{
+    public class RecetaViewModel
+    {
+        public Receta Receta { get; set; } //entidad de la bd
+        public IEnumerable<Receta> TresRecetas { get; set; } 
+        //No requiero cargar toda la informacion
+
+    }
+}
